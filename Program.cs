767a9d48b5e5daca9769337b590783d221e5fc03
Jeffry1990 <109137757+Jeffry1990@users.noreply.github.com>
2024-12-04 14:30:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp44
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int gold;
+            int crystal;
+            int crystalOfCost = 5;
+
+            Console.WriteLine("Добро пожаловать в наш магазин! Здесь вы можете купить кристаллы за 5 золотых");
+            Console.Write("Сколько у вас золота? ");
+            gold = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Сколько кристаллов Вы хотели купить? ");
+            crystal = Convert.ToInt32(Console.ReadLine());
+
+            gold -= crystalOfCost * crystal;
+
+            Console.WriteLine($"У вас {gold} золота и {crystal} кристаллов");
+        }
+    }
+}
