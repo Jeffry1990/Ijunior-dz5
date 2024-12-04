d1d1973035125c01890914ff6387e2bbd54fee49
Jeffry1990 <109137757+Jeffry1990@users.noreply.github.com>
2024-12-04 14:44:47 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@
             int crystal;
             int crystalOfCost = 5;
 
-            Console.WriteLine("Добро пожаловать в наш магазин! Здесь вы можете купить кристаллы за 5 золотых");
+            Console.WriteLine($"Добро пожаловать в наш магазин! Здесь вы можете купить кристаллы за {crystalOfCost} золотых");
             Console.Write("Сколько у вас золота? ");
             gold = Convert.ToInt32(Console.ReadLine());
             Console.Write("Сколько кристаллов Вы хотели купить? ");
